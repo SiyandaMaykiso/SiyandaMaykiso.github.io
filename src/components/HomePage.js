e1d3d512fcbb36bb6764ad00989236dd90fc1e78
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-04-26 17:33:24 -0500

    Add responsive CSS

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,24 @@
-// HomePage.js
 import React from 'react';
+import './HomePage.css'; // Ensure you have a CSS file for styles specific to the homepage
 
 function HomePage() {
   return (
     <div className="home-page">
-      <h1>About Me</h1>
-      <p>This is the about section where you can learn more about my background, skills, and experiences.</p>
+      <section className="intro-section">
+        <img src="url_to_your_professional_photo.jpg" alt="Professional Portrait" className="profile-photo"/>
+        <h1>About Me</h1>
+        <p>I am a Full-Stack Web Developer and a seasoned digital marketing professional with an MBA from Hamline University. Skilled in crafting code and compelling content to empower and engage audiences across various platforms.</p>
+      </section>
+      <section className="detailed-background">
+        <h2>Professional Background</h2>
+        <p>Experienced in web technologies like HTML, CSS, JavaScript, Node.js, React, and PostgreSQL. I specialize in web performance analysis, optimization, and content marketing, driving increases in website traffic and lead generation.</p>
+        <p>My journey has included significant roles in paid media, email marketing, public relations, social media, and search engine optimization (SEO). I am proficient in analyzing metrics to refine campaigns and provide actionable insights to stakeholders.</p>
+        <p>My collaborative approach enables me to work effectively with cross-functional teams and external partners, fostering innovation and achieving remarkable results. I stay at the forefront of industry trends, continually expanding my knowledge to bring cutting-edge strategies to every role.</p>
+      </section>
+      <section className="achievements">
+        <h2>Impact and Achievements</h2>
+        <p>Throughout my career, I have consistently demonstrated a commitment to delivering tangible, measurable outcomes and driving growth for organizations. My passion for marketing and dedication to excellence make me an asset in any digital marketing environment.</p>
+      </section>
     </div>
   );
 }
