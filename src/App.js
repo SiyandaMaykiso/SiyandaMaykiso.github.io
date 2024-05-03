25f7cc12c375f3e1d774b5ea1ca781b732ecae9f
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-05-03 05:15:17 -0500

    Portfolio clean up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-// App.js
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
