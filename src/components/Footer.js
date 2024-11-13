fb08929e94b512c80b1d5b2f8a8c7cfaa27a4b2a
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-11-13 14:46:45 -0600

    Change URL

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
         <a href="https://github.com/SiyandaMaykiso" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-github"></i>
         </a>
-        <a href="https://www.linkedin.com/in/siyanda-mayekiso-mba/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/siyanda-burnham-mba/" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-linkedin"></i>
         </a>
       </div>
