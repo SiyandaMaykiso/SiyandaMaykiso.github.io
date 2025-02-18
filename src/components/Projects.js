9febaa2c88034e1cc54d9d390ed0e73299c2e087
Siyanda Mayekiso <s.mayekiso@gmail.com>
2025-02-18 12:45:54 -0600

    Add new file to portfolio

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,16 @@ const projects = [
 
   {
     id: 1,
+    title: 'AI-Powered Code Reviwer',
+    description: 'The AI-Powered Content Summarizer is a web application designed to generate absorbable summaries of long-form content using advanced natural language processing (NLP) technology. This project aims to streamline information sharing by reducing large articles into brief, easy-to-digest summaries, turning academic and business articles into short-form content. The frontend is built with React, and the backend is powered by Node.js with a PostgreSQL database hosted on Heroku.',
+    image: 'https://github.com/SiyandaMaykiso/AI-Powered-Code-Reviewer/blob/main/screenshots/AI_Powered_Code_Reviewer_Review_Page.png',
+    repo: 'https://github.com/SiyandaMaykiso/AI-Powered-Code-Reviewer',
+    live: 'https://ai-powered-code-reviewer-c7e564a6a3d0.herokuapp.com/',
+    date: new Date('2024-07-01')
+},
+
+  {
+    id: 2,
     title: 'AI-Powered Content Summarizer',
     description: 'The AI-Powered Content Summarizer is a web application designed to generate absorbable summaries of long-form content using advanced natural language processing (NLP) technology. This project aims to streamline information sharing by reducing large articles into brief, easy-to-digest summaries, turning academic and business articles into short-form content. The frontend is built with React, and the backend is powered by Node.js with a PostgreSQL database hosted on Heroku.',
     image: 'https://github.com/SiyandaMaykiso/AI-Powered-Content-Summarizer/blob/main/screenshots/Summarize_Page_AI_Content_Powered_Summarizer.png?raw=true',
@@ -14,7 +24,7 @@ const projects = [
 },
 
   {
-    id: 2,
+    id: 3,
     title: 'AI-Powered Chatbot',
     description: 'The AI-Powered Chatbot is an intelligent web application that simulates human-like conversations using advanced NLP (Natural Language Processing). Built using React, Node.js, and PostgreSQL, it integrates OpenAI’s GPT API to provide real-time responses and manage user interactions, with features like authentication and persistent chat history.',
     image: 'https://github.com/SiyandaMaykiso/AI-Powered-Chatbot/blob/main/screenshots/Chat_Window_Screenshot.png?raw=true',
@@ -24,7 +34,7 @@ const projects = [
 },
   
   { 
-    id: 3,
+    id: 4,
     title: 'Recipe Sharing Platform',
     description: 'The Recipe Sharing Platform is a full-stack web application that allows users to share their favorite recipes, complete with ingredients, cooking instructions, and images. Built using React, Node.js, and PostgreSQL.',
     image: 'https://github.com/SiyandaMaykiso/SiyandaMaykiso.github.io/blob/main/Screenshots/Recipe-Sharing-Platform-RecipeListing%20Page.png?raw=true',
@@ -35,7 +45,7 @@ const projects = [
   
 
   { 
-    id: 4, 
+    id: 5, 
     title: 'To-Do List Application',
     description: 'This To-Do List application is a user-friendly full-stack platform that allows users to manage their daily tasks efficiently. Users can easily add tasks to their personal list, mark tasks as completed, and delete them as needed. Built using React, Node.js and PostgreSQL.',
     image: 'https://github.com/SiyandaMaykiso/SiyandaMaykiso.github.io/blob/main/Screenshots/To-Do%20List%20Application%20Dashboard.png?raw=true', 
@@ -45,7 +55,7 @@ const projects = [
   },
 
   { 
-    id: 5, 
+    id: 6, 
     title: 'Personal Blog Platform',
     description: 'The Personal Blog Platform is a dynamic full stack web application designed for writers, bloggers, and creators who wish to publish, manage, and share their writings. This platform offers a sleek user interface for an engaging writing and reading experience. Built using React, Node.js and PostgreSQL.', 
     image: 'https://github.com/SiyandaMaykiso/SiyandaMaykiso.github.io/blob/main/Screenshots/Create%20Post%20Screenshot.png?raw=true', 
@@ -55,7 +65,7 @@ const projects = [
   },
  
   {
-    id: 6, 
+    id: 7, 
     title: 'What is Cooking Project', 
     description: 'This frontend application that gives you recipes based on the ingredients you have on hand, and helps you find grocery stores nearby.', 
     image: 'https://github.com/SiyandaMaykiso/SiyandaMaykiso.github.io/blob/main/Screenshots/What%20is%20cooking%20project%20homepage%20screenshot.png?raw=true', 
@@ -65,7 +75,7 @@ const projects = [
   },
 
   {
-    id: 7, 
+    id: 8, 
     title: 'JavaScript WeatherApp', 
     description: 'This is a frontend practical weather app made with JavaScript that tells you the weather in your current location. Made with HTML, CSS, and JavaSript. This application uses the OpenWeather API.', 
     image: 'https://github.com/SiyandaMaykiso/SiyandaMaykiso.github.io/blob/main/Screenshots/Screenshot%20Current%20Weather%20App.png?raw=true', 
@@ -75,7 +85,7 @@ const projects = [
   },
 
   {
-    id: 8, 
+    id: 9, 
     title: 'Workday Scheduler', 
     description: 'This is a frontend workday calendar application that allows the user to save events for each hour of the day. This app runs in the browser and features dynamically updated HTML, CSS, jQuery and Moment.js.', 
     image: 'https://github.com/SiyandaMaykiso/SiyandaMaykiso.github.io/blob/main/Screenshots/Workday%20Scheduler%20Screenshot.png?raw=true', 
@@ -85,7 +95,7 @@ const projects = [
   },
 
   {
-    id: 9, 
+    id: 10, 
     title: 'Password Generator', 
     description: 'This frontend password generator contains, HTML, Javascript code and CSS styling. This app will run sucessfully on different browsers and store secure information. It contains a clear and polished user interface that is responsive.', 
     image: 'https://github.com/SiyandaMaykiso/SiyandaMaykiso.github.io/blob/main/Screenshots/Password%20Generator%20Screenshot.png?raw=true', 
@@ -95,7 +105,7 @@ const projects = [
   },
 
   {
-    id: 10, 
+    id: 11, 
     title: 'Movie Guide', 
     description: 'This app will help you find any theatrical movie, the app will also provide you with a movie rating. This app works on most desktop and mobile applications. Built with the Open Movie Database API (The OMDb API is a RESTful web service to obtain movie information), Poppins Font, HTML, CSS & JavaScript', 
     image: 'https://github.com/SiyandaMaykiso/SiyandaMaykiso.github.io/blob/main/Screenshots/Movie%20Guide%20Screenshot.png?raw=true', 
